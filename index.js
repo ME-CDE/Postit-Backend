304183d983a8cf79765622c33b837cb118a94066
ME-CDE <lewiswalter756.com>
2022-09-15 02:38:26 +0100

    a

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const connect =  require("./db/connect")
 const router = require("./router/userRouter")
 const cookieParser = require("cookie-parser");
-app.use(cors({origin: 'https://postiit.netlify.app',credentials:true, exposedHeaders:['Set-Cookie', 'Date', 'ETag']}))
+app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cookieParser())
